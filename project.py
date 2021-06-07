18849ead07b9a995420843b0528a0922e14b8608
Vigneshkumar <60309361+Vigneshkumar212@users.noreply.github.com>
2021-06-07 22:25:54 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,6 @@
+file1Data = open('File1.txt', 'r') 
+file2Data = open('File2.txt', 'r') 
+file1Data = file1Data.read()
+file2Data = file2Data.read()
+open("File1.txt", "w").write(file2Data)
+open("File2.txt", "w").write(file1Data)
