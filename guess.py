544d988d1c0cdde420978d8284bdf3f956aa0385
Vigneshkumar <60309361+Vigneshkumar212@users.noreply.github.com>
2021-06-25 16:09:32 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -6,7 +6,6 @@ print("number gzuessing game")
 print("Guess a number between 1 and 9")
 #making a random no bwt 1-9
 randNo =(random.choice([1,2,3,4,5,6,7,8,9,5,6,7,3,4,7,2,4,5,8,9,2,3,3,2,4,5,8,6,4]))
-print(randNo)
 #an infinite loop
 while True:
     #inputting the guessed no
