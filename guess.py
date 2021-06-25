6abe974537d5524eac18921fa31220a2092be05f
Vigneshkumar <60309361+Vigneshkumar212@users.noreply.github.com>
2021-06-25 16:08:35 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,28 @@
+#start
+#importing random
+import random;
+#the prints
+print("number gzuessing game")
+print("Guess a number between 1 and 9")
+#making a random no bwt 1-9
+randNo =(random.choice([1,2,3,4,5,6,7,8,9,5,6,7,3,4,7,2,4,5,8,9,2,3,3,2,4,5,8,6,4]))
+print(randNo)
+#an infinite loop
+while True:
+    #inputting the guessed no
+    no = input("Enter your guess :-")
+    #if higher, we say it
+    if int(no) > int(randNo):
+        print("Your guess was too high: Guess a number lesser than "+no)
+    
+    #if lower, we say it
+    if int(no) < int(randNo):
+        print("Your guess was too low: Guess a number more than "+no)
+    
+    #if same, we say it. and Congragulate
+    if int(no) == int(randNo):
+        print("YAY!!! You found the no")
+        #break the prog
+        break
+
+#END
